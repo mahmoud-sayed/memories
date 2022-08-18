cd060273df80de5f53002d15b9f8f89ec186a6ba
Mahmoud AlBoraey <mahmoudalsayied@gmail.com>
2022-08-18 17:24:19 +0200

    fixed issue

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postsRouts from './routes/posts.js';
+import connectionURL from './DBConnectionURL.js';
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 //DB connection 
-const connectionURL = 'mongodb+srv://admin:u0wqqZZYozcCZpvS@cluster0.ik4ydce.mongodb.net/?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(connectionURL)
