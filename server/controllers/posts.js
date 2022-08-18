207acab99ddba676e72068f249046d73f908b9b4
Mahmoud AlBoraey <mahmoudalsayied@gmail.com>
2022-08-18 17:05:37 +0200

    done making the database and connection betwean tadabase and backend code and started to create posts front end

---
+++ server/controllers/posts.js
@@ -0,0 +1,25 @@
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+
+    const postMessages = await PostMessage.find();
+    console.log(postMessages);
+    res.status(200).json(postMessages);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+  res.send('this working');
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = PostMessage(post);
+
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
